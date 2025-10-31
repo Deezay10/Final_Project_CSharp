490a57fb6b27faf7928026152ef1f0eb4bd9289d
Deezay10 <nino.caillaud@gmail.com>
2025-10-31 10:42:17 +0100

    Creation Class Client

--- Projet_Final/Model/Client.cs
+++ Projet_Final/Model/Client.cs
@@ -1,6 +1,24 @@
-﻿namespace Projet_Final.Model;
+﻿using System;
+using Projet_Final;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+namespace Projet_Final.Model;
 
 public class Client
 {
+    [Key]
+    public Guid Id { get; set; } = Guid.NewGuid();
     
+    public string Lastname { get; set; }
+    
+    public string Firstname { get; set; }
+    
+    public string Birthdate { get; set; }
+    
+    public string PhoneNumber { get; set; }
+    
+    public string Email { get; set; }
+    
+    // Relation 1..n : un client peut avoir plusieurs voitures
+    public ICollection<Car> Cars { get; set; } = new List<Car>();
 }
