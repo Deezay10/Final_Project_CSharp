73e7de9c98a40d622ac347287c03c4f7fc3c480b
Deezay10 <nino.caillaud@gmail.com>
2025-10-31 11:25:28 +0100

    Add DbConnection and Db Context v1

--- Projet_Final/Program.cs
+++ Projet_Final/Program.cs
@@ -4,9 +4,23 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Npgsql;
+using Projet_Final.Interface;
+using DbContext = Projet_Final.Interface.DbContext;
 
 //créer un lien vers appsetting.json
 var configuration = new ConfigurationBuilder()
     .SetBasePath(Directory.GetCurrentDirectory())
     .AddJsonFile(@"Projet_Final/appsettings.json", optional: false, reloadOnChange: true)
     .Build();
+    
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddDbContext<DbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+        
+        services.AddTransient<DbConnection>();
+    })
+    .Build();
+    
+    
