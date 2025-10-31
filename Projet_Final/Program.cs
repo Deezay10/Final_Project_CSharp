c72bec5305457da8a668fea34bc9db0948fc3ebe
Vladimir-clic <vladimir.vitrou@supdevinci-edu.fr>
2025-10-31 10:32:31 +0100

    erreur commit adresse addjsonfile

--- Projet_Final/Program.cs
+++ Projet_Final/Program.cs
@@ -3,5 +3,5 @@
 //créer un lien vers appsetting.json
 var configuration = new ConfigurationBuilder()
     .SetBasePath(Directory.GetCurrentDirectory())
-    .AddJsonFile(@"C:\\Users\\julie\\RiderProjects\\CoursSupDeVinci\\CoursSupDeVinci\\appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile(@"Projet_Final/appsettings.json", optional: false, reloadOnChange: true)
     .Build();
