d20f75883f0933312dba07e37c36446dd7aff742
Deezay10 <nino.caillaud@gmail.com>
2025-10-31 12:23:55 +0100

    Add all client in clients

--- Projet_Final/Program.cs
+++ Projet_Final/Program.cs
@@ -5,6 +5,8 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Npgsql;
 using Projet_Final.Interface;
+using Projet_Final.Model;
+using Projet_Final.Utils;
 
 //créer un lien vers appsetting.json
 var configuration = new ConfigurationBuilder()
@@ -34,13 +36,33 @@ for (int i = 1; i < lignes.Length; i++)
 {
     String line = lignes[i];
     Car car = new Car();
-    
+
     person.Lastname = line.Split(',')[1];
     person.Firstname = line.Split(',')[2];
     person.Birthdate = DateTimeUtils.ConvertToDateTime(line.Split(',')[3]);
     person.Size = Int32.Parse(line.Split(',')[5]);
-    
+
     List<String> details = line.Split(',')[4].Split(';').ToList();
-    
+
 
     car.Add(Car);
+
+    List<Client> clients = new List<Client>();
+
+    var lignes_client = File.ReadAllLines(path);
+
+    for (int f = 1; f < lignes.Length; f++)
+    {
+        String client_line = lignes[f];
+        Client client = new Client();
+
+        client.Lastname = line.Split('%')[0];
+        client.Firstname = line.Split('%')[1];
+        client.Birthdate = DateTimeUtils.ConvertToDateTime(line.Split('%')[2]);
+        client.PhoneNumber = line.Split('%')[3];
+        client.Email = line.Split('%')[4];
+
+        clients.Add(client);
+        
+    }
+}
