35cd68f4ca96eee3f74a3026b29bd262342fe63d
Vladimir-clic <vladimir.vitrou@supdevinci-edu.fr>
2025-10-31 10:30:53 +0100

    test installation nuget

--- Projet_Final/Program.cs
+++ Projet_Final/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+
+//créer un lien vers appsetting.json
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile(@"C:\\Users\\julie\\RiderProjects\\CoursSupDeVinci\\CoursSupDeVinci\\appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
