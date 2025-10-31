b73e33ef6f107ddbc495609eb15c5eb344d01828
Vladimir-clic <vladimir.vitrou@supdevinci-edu.fr>
2025-10-31 11:00:01 +0100

    modifications

---
+++ Projet_Final/Interface/DbConnection.cs
@@ -0,0 +1,27 @@
+﻿namespace Projet_Final.Interface;
+using Npgsql;
+
+
+//code du formateur à adapter à nos besoins
+
+//déclare une classe DbConnection accessible dans d'autres parties du code
+public class DbConnection
+{
+    //création d'une variable en lecture seule
+    private readonly SchoolDbContext _schoolDbContext;
+    
+    // constructeur de la classe DbConnection
+    public DbConnection(SchoolDbContext schoolDbContext)
+    {
+        _schoolDbContext = schoolDbContext;
+    }
+
+    //ajoute l'objet maClasse dans la base de donnée
+    public void SaveFullClasse(Classe maClasse)
+    {
+        _schoolDbContext.Add(maClasse);
+        
+        _schoolDbContext.SaveChanges();
+    }
+
+}
--- Projet_Final/Program.cs
+++ Projet_Final/Program.cs
@@ -1,4 +1,9 @@
-﻿
+﻿using Projet_Final;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
 
 //créer un lien vers appsetting.json
 var configuration = new ConfigurationBuilder()
