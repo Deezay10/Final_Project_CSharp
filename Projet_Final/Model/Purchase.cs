d513260682a79a4453ca50d45be2c9c4859f53f9
Deezay10 <nino.caillaud@gmail.com>
2025-11-09 15:43:35 +0100

    creation nouvelle table Purchase

---
+++ Projet_Final/Model/Purchase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Projet_Final;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Projet_Final.Model;
+
+public class Purchase
+{
+    [Key] 
+    public Guid Id { get; set; } = Guid.NewGuid();
+    
+    public string car_brand { get; set; }
+    
+    public string car_model { get; set; }
+    
+    public string client_lastname { get; set; }
+    
+    public string client_email { get; set; }
+    
+    
+    [ForeignKey("Client")]
+    public Guid? ClientId { get; set; }
+    
+    public Client? Client { get; set; }
+    
+    [ForeignKey("Car")]
+    public Guid? CarId { get; set; }
+    
+    public Car? Car { get; set; }
+}
